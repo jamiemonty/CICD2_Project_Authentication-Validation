3f17d6c810ababf4cd1df518f8f51b8f7af2ee83
JamieMonty <G00419525@atu.ie>
2025-10-10 16:05:45 +0100

    Added code to hash the pw, added API endpoint to POST details and register.

--- docu_serve/main.py
+++ docu_serve/main.py
@@ -1,7 +1,14 @@
 # app/main.py
+
 from fastapi import FastAPI, HTTPException, status
 from .schemas import User, UserBase, UserCreate
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "JAMIESKEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
 
@@ -14,6 +21,12 @@ def hash_password(password: str):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 @app.get("/api/users")
 def get_users():
@@ -33,4 +46,12 @@ def add_user(user: User):
     users.append(user)
     return user
 
-
+@app.post("/api/users/register", status_code=status.HTTP_201_CREATED)
+def register_user(user:UserCreate):
+    if any(u.email == user.email for u in users):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already exists")
+    user_id = len(users) + 1
+    hashed_password = hash_password(user.password)
+    new_user = User(user_id=user_id, name=user.name, email=user.email, age=user.age, hashed_password=hashed_password)
+    users.append(new_user)
+    return {"msg":"USer registered successfully", "user_id": user_id}
