3a12bac8437d23b5a5be63f3c34235eef5a121a9
JamieMonty <G00419525@atu.ie>
2025-10-10 14:53:58 +0100

    Installed required libraries (secure pw hashing, JWT creation, verification, login form support. Fixed schema registration so the password is not stored in the same place as user information. Hashed the password so it is not stored in plain-text.

--- docu_serve/schemas.py
+++ docu_serve/schemas.py
@@ -1,9 +1,14 @@
 # app/schemas.py
 from pydantic import BaseModel, EmailStr, constr, conint
 
-class User(BaseModel):
-    user_id: int
+class UserBase(BaseModel):
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     age: conint(gt=18)
-    password: costr(min_length = 8, max_length = 60, pattern=r'^(?=.*[A-Z])(?=.*\d).*$')
+
+class UserCreate(UserBase):
+     password: constr(min_length=8, max_length=60, pattern=r'^(?=.*[A-Z])(?=.*\d).*$')
+
+class User(UserBase):
+     user_id: int
+     hashed_password: str
